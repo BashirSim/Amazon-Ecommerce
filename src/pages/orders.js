d76a7812394ed1be620e5da727750cc0fe1f7c7e
Bashir <zi10zo95@gmail.com>
2021-07-06 15:18:09 +0300

    'v1.05'

--- src/pages/orders.js
+++ src/pages/orders.js
@@ -1,7 +1,10 @@
+import { useSession } from "next-auth/client";
 import Header from "../components/Header";
 
-const Orders = () => {
+// We're passing orders as an argument to do a Server Side Rendering(SSR)
+const Orders = ({ orders }) => {
   // We are importing the session to know if the user is logged in or not
+  const [session] = useSession();
 
   return (
     <div>
@@ -10,6 +13,12 @@ const Orders = () => {
         <h1 className="text-3xl border-b mb-2 pb-1 border-yellow-400">
           Your Orders
         </h1>
+        {session ? (
+          <h2>x Orders</h2>
+        ) : (
+          <h2>Please sign in to see your orders</h2>
+        )}
+        <div className="mt-5 space-y-4"></div>
       </main>
     </div>
   );
